cbadedd2b8913dbdb9f15f842c83b1e9e2330363
Marina Manrique <mmanrique@era7.com>
2015-07-23 16:57:41 +0200

    update install method

--- src/main/scala/samtools.scala
+++ src/main/scala/samtools.scala
@@ -3,7 +3,7 @@ package ohnosequencesBundles.statika
 import ohnosequences.statika._, bundles._, instructions._
 
 
-abstract class samtools(val samtoolsVersion: String, val bcftoolsVersion: String) extends Bundle(cdevel, compressinglibs, ncurses) {
+abstract class Samtools(val samtoolsVersion: String) extends Bundle(cdevel, compressinglibs, ncurses) {
 
   def install: Results = {
     Seq("wget", s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/samtools/${samtoolsVersion}/samtools-${samtoolsVersion}.tar.bz2") ->-
@@ -11,16 +11,11 @@ abstract class samtools(val samtoolsVersion: String, val bcftoolsVersion: String
     Seq("cd", s"samtools-${samtoolsVersion}") ->-
     Seq("make") ->-
     Seq("cp", "samtools", "/usr/bin/") ->-
-    Seq("cd", "..") ->-
-    Seq("wget", s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/bcftools/${bcftoolsVersion}/bcftools-${bcftoolsVersion}.tar.bz2") ->-
-    Seq("tar", "--bzip2", "-xf", s"bcftools-${bcftoolsVersion}.tar.bz2") ->-
-    Seq("cd", s"bcftools-${bcftoolsVersion}") ->-
-    Seq("make") ->-
-    //Not sure if needed
-    //Seq("make", "install") ->-
-    Seq("cp", "bcftools", "/usr/bin/") ->-
-    Seq("cp", "vcfutils.pl", "/usr/bin/") ->-
     success(s"${bundleName} is installed")
   }
 
+
+
 }
+
+
