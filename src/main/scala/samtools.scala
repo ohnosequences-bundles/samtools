6bb288a61b6931916f78c44f2431269aedd2d5e4
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-07-23 21:07:14 +0200

    improve install process, tested and working

--- src/main/scala/samtools.scala
+++ src/main/scala/samtools.scala
@@ -7,22 +7,20 @@ abstract class Samtools(val samtoolsVersion: String) extends Bundle(cdevel, comp
 
   import ammonite.ops._
 
-  val samtoolsDir = s"samtools-${samtoolsVersion}"
+  val samtoolsDir     = s"samtools-${samtoolsVersion}"
+  val samtoolsBinary  = samtoolsDir/"samtools"
+  val bin             = root/"usr"/"bin"
 
-  def install: Results = {
-    Seq(
-      "wget", s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/samtools/${samtoolsVersion}/${samtoolsDir}.tar.bz2"
-    ) ->-
+  final def install: Results = {
+
+    val wd = cwd
+
+    Seq("wget", s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/samtools/${samtoolsVersion}/${samtoolsDir}.tar.bz2") ->-
     Seq("tar", "--bzip2", "-xf", s"${samtoolsDir}.tar.bz2") ->-
-    {
-      val wd = cwd
-      %cd (wd/samtoolsDir).toString
-    } ->-
-    Seq("make") ->-
-    Seq("cp", "samtools", "/usr/bin/") ->-
-    success(s"${bundleName} is installed")
+    Seq("make", "-C", (wd/samtoolsDir).toString) ->- {
+
+      cp(wd/samtoolsBinary, bin/"samtools")
+      success(s"${bundleName} is installed")
+    }
   }
-
-
-
 }
